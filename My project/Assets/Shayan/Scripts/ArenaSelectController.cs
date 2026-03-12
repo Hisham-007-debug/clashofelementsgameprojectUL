c523fa7fab468ecc9d91f180a8b6cc5ee8e84ed6
reznon <shayankhan9105@gmail.com>
2026-03-12 15:57:32 +0000

    New scene updates

---
+++ My project/Assets/Shayan/Scripts/ArenaSelectController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ArenaSelectController : MonoBehaviour
+{
+    public Image arenaPreview;
+    public TMP_Text arenaName;
+
+    public Sprite fireArena;
+    public Sprite iceArena;
+    public Sprite airArena;
+    public Sprite earthArena;
+
+    private string selectedArena = "";
+
+    public void SelectFireArena()
+    {
+        arenaPreview.sprite = fireArena;
+        arenaName.text = "FIRE ARENA";
+        selectedArena = "Fire";
+    }
+
+    public void SelectIceArena()
+    {
+        arenaPreview.sprite = iceArena;
+        arenaName.text = "ICE ARENA";
+        selectedArena = "Ice";
+    }
+
+    public void SelectAirArena()
+    {
+        arenaPreview.sprite = airArena;
+        arenaName.text = "AIR ARENA";
+        selectedArena = "Air";
+    }
+
+    public void SelectEarthArena()
+    {
+        arenaPreview.sprite = earthArena;
+        arenaName.text = "EARTH ARENA";
+        selectedArena = "Earth";
+    }
+
+    public void StartFight()
+    {
+        if (selectedArena == "") return;
+        SceneManager.LoadScene("FightScene");
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+}
